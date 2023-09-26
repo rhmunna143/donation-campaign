468703e227730696cfe5341bf41e3986c6c51a78
rhmunna143 <rhmunna19@gmail.com>
2023-09-26 14:27:35 +0600

    creating PieChart

---
+++ src/components/PiChart/PiChart.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Pie, PieChart, Cell, Label, LabelList } from "recharts";
+
+const PaiChart = ({ maxLength, length }) => {
+    // Calculate the percentage of length and maxLength with one decimal place
+    const lengthPercentage = ((length / maxLength) * 100).toFixed(1);
+    const maxLengthPercentage = (100 - lengthPercentage).toFixed(1);
+
+    // Data for the Pie Chart
+    const data = [
+        { name: "Length", value: parseFloat(lengthPercentage) },
+        { name: "MaxLength", value: parseFloat(maxLengthPercentage) },
+    ];
+
+    // Define custom colors for the Pie segments
+    const colors = ["#0088FE", "#FF8042"];
+
+    return (
+        <div>
+            <PieChart width={750} height={300}>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    cx={150}
+                    cy={150}
+                    outerRadius={100}
+                    fill="#8884d8"
+                    labelLine={false} // Disable the label line connecting to the center
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index]} />
+                    ))}
+                    <LabelList
+                        data={data}
+                        position="inside"
+                        content={({ value, name }) => `${name}: ${value.toFixed(1)}%`}
+                        fill="#FFFFFF"
+                        fontSize={16}
+                    />
+                </Pie>
+            </PieChart>
+        </div>
+    );
+};
+
+export default PaiChart;
--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,9 +1,18 @@
+import PaiChart from "../../components/PiChart/PiChart";
+
 
 
 const Statistics = () => {
+    const maxLength = 12;
+
+    const primaryDonated = JSON.parse(localStorage.getItem("donated"));
+    const length = primaryDonated.length;
+
+
+
     return (
-        <div>
-            tatistic page
+        <div className="max-w-6xl mx-auto">
+            <PaiChart maxLength={maxLength} length={length}></PaiChart>
         </div>
     );
 };
