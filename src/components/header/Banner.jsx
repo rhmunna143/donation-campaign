1fbcd2b7c111d7e2ae75212956cc28c95e885680
rhmunna143 <rhmunna19@gmail.com>
2023-09-26 19:16:54 +0600

    search engine functionality done

--- src/components/header/Banner.jsx
+++ src/components/header/Banner.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import "./banner.css"
 
-const Banner = () => {
+const Banner = ({handleSearchClick}) => {
+    const [value, setValue] = useState("")
+
+    const handleSearchInput = (event) => {
+        const inputValue = event.target.value;
+
+        setValue(inputValue)
+    }
+    
 
     return (
         <div className="flex flex-col justify-center items-center gap-10 pt-14 banner-bg h-[85vh] p-10 md:p-0 w-full">
             <h2 className="text-4xl font-bold">I Grow By Helping People In Need</h2>
             <div className="form-control">
                 <div className="input-group items-center">
-                    <input type="text" placeholder="Search here…" className="input w-[250px] md:w-[280px] h-[50px] input-bordered rounded-md border-r-0" />
-                    <button className="btn border-none bg-[#FF444A] text-white rounded-md py-0 hover:bg-black capitalize font-semibold">
+                    <input onChange={handleSearchInput} type="text" placeholder="Search here…" className="input w-[250px] md:w-[280px] h-[50px] input-bordered rounded-md border-r-0" />
+                    <button onClick={() => handleSearchClick(value)} className="btn border-none bg-[#FF444A] text-white rounded-md py-0 hover:bg-black capitalize font-semibold">
                         Search
                     </button>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../components/header/Banner";
 import HomeCard from "../components/HomeCard/HomeCard";
+import { useState } from "react";
 
 
 
 const Home = () => {
     const data = useLoaderData()
+    const [searchResult, setSearchResult] = useState(data)
+
+    const handleSearchClick = (value) => {
+        const searchFilter = data?.filter(item => item.category == value)
+        
+        setSearchResult(searchFilter)
+    }
 
     return (
         <div className="rounded-md">
-            <Banner></Banner>
+            <Banner handleSearchClick={handleSearchClick}></Banner>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl mx-auto my-20 p-4 lg:p-0">
-                {data?.map(item => <HomeCard key={item.id} data={item}></HomeCard>
+                {searchResult?.map(item => <HomeCard key={item.id} data={item}></HomeCard>
                 )}
             </div>
         </div>
